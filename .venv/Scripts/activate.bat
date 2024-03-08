c260a2a9c8f34262e2a78509ff32665515e754fe
lukeC01e5 <colesluke@gmial.com>
2024-03-08 20:46:51 +1300

    update flask stuff in new locatio

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\OEM\python-flask-mongodb-projects\rest-api-server\.venv
+set VIRTUAL_ENV=C:\Users\OEM\Documents\GitHub\gameapi\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
